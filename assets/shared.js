3b204d9d04335629429489aba70b65ea69e4607c
Jon Thielen <github@thielen.science>
2021-03-26 15:27:25 -0500

    bad placefile links

--- assets/shared.js
+++ assets/shared.js
@@ -6,7 +6,7 @@ function loadPlacefileLinks(api_base) {
     var chase_base_url = "https://chase.iawx.info/";
     var team_id = localStorage.getItem("team_id");
 
-    $("#team-current").val(chase_base_url + api_base + "placefile/" + team_id + "/current/content");
-    $("#team-tracks").val(chase_base_url + api_base + "placefile/" + team_id + "/tracks/content");
-    $("#team-history").val(chase_base_url + api_base + "placefile/" + team_id + "/history/content");
+    $("#team-current").val(chase_base_url + api_base + "placefile/team/" + team_id + "/current/content");
+    $("#team-tracks").val(chase_base_url + api_base + "placefile/team/" + team_id + "/tracks/content");
+    $("#team-history").val(chase_base_url + api_base + "placefile/team/" + team_id + "/history/content");
 }

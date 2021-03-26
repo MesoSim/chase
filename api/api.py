af5798c7e71f6dbbbd845682dec37ec06607b0ed
Jon Thielen <github@thielen.science>
2021-03-26 16:10:46 -0500

    allow lsr placefile to be valid when the table isn't there yet

--- api/api.py
+++ api/api.py
@@ -465,29 +465,33 @@ class PlacefileLsrContent(Resource):
 
         hours_valid = config.lsr_hours_valid  # LSR Validity (archive time)
         remark_wrap_length = int(config.get_config_value("lsr_remark_wrap_length"))  # Text Wrapping
-        
-        lsr_con = sql.connect(lsr_db_file)
-        lsr_cur = lsr_con.cursor()
-        
-        # Prep the time interval (arc time)
-        t1 = arc_time_from_cur(datetime.now(tz=pytz.UTC), timings=config.timings)
-        t0 = t1 - timedelta(hours=hours_valid)
-        t0, t1 = (t.strftime(std_fmt) for t in [t0, t1])
 
-        # Get the data
-        lsr_cur.execute("SELECT * FROM lsrs_raw WHERE valid BETWEEN ? AND ?", [t0, t1])
-        lsrs_raw = lsr_cur.fetchall()
+        try:
+            lsr_con = sql.connect(lsr_db_file)
+            lsr_cur = lsr_con.cursor()
+            
+            # Prep the time interval (arc time)
+            t1 = arc_time_from_cur(datetime.now(tz=pytz.UTC), timings=config.timings)
+            t0 = t1 - timedelta(hours=hours_valid)
+            t0, t1 = (t.strftime(std_fmt) for t in [t0, t1])
 
-        # Scale the data to cur time
-        lsrs_scaled = scale_raw_lsr_to_cur_time(lsrs_raw, timings=config.timings)
+            # Get the data
+            lsr_cur.execute("SELECT * FROM lsrs_raw WHERE valid BETWEEN ? AND ?", [t0, t1])
+            lsrs_raw = lsr_cur.fetchall()
 
-        # Output the LSRs
-        for lsr_tuple in lsrs_scaled:
-            output += gr_lsr_placefile_entry_from_tuple(
-                lsr_tuple,
-                wrap_length=remark_wrap_length,
-                tz=pytz.timezone(config.get_config_value("pytz_timezone"))
-            ) + '\n'
+            # Scale the data to cur time
+            lsrs_scaled = scale_raw_lsr_to_cur_time(lsrs_raw, timings=config.timings)
+
+            # Output the LSRs
+            for lsr_tuple in lsrs_scaled:
+                output += gr_lsr_placefile_entry_from_tuple(
+                    lsr_tuple,
+                    wrap_length=remark_wrap_length,
+                    tz=pytz.timezone(config.get_config_value("pytz_timezone"))
+                ) + '\n'
+        except:
+            # Just have the header matter if errors
+            pass
         
         response = make_response(output)
         response.headers['content-type'] = 'text/plain'

cf8e37d8b1ad83d1943aaef83a7c5bd50fb64fed
Jon Thielen <github@thielen.science>
2022-03-27 08:17:58 -0600

    minor updates to allow for flexible hostname (still can't be in subdir, but oh well)

--- assets/shared.js
+++ assets/shared.js
@@ -1,12 +1,19 @@
 /* Shared JS functions across pages in the chase applet */
 /* jQuery must be loaded first */
 
-function loadPlacefileLinks(api_base) {
-    // Note: these are considered static, so just set the team-dependent ones
-    var chase_base_url = "https://chase.iawx.info/";
+function loadPlacefileLinks(chase_base_url, api_base) {
+    // Update all!
+    // TODO: allow for warnings and level 2 to be located elsewhere, not as subdirectory
+    //       that probably means loading these data from API, but oh well.
     var team_id = localStorage.getItem("team_id");
 
+    $("#l2-source").val(chase_base_url + "l2/");
+    $("#warnings-source").val(chase_base_url + "warnings/");
+    $("#lsr-placefile").val(chase_base_url + api_base + "placefile/lsr/content");
     $("#team-current").val(chase_base_url + api_base + "placefile/team/" + team_id + "/current/content");
     $("#team-tracks").val(chase_base_url + api_base + "placefile/team/" + team_id + "/tracks/content");
     $("#team-history").val(chase_base_url + api_base + "placefile/team/" + team_id + "/history/content");
+    $("#all-current").val(chase_base_url + api_base + "placefile/team/current/content");
+    $("#all-tracks").val(chase_base_url + api_base + "placefile/team/tracks/content");
+    $("#all-history").val(chase_base_url + api_base + "placefile/team/history/content");
 }
